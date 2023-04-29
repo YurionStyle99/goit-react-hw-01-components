e3596fba550216d37c2f733b696138b29deb1def
YurionStyle <74459224+YurionStyle@users.noreply.github.com>
2023-04-29 14:45:27 +0200

    2 task

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,26 @@
+import css from "./Statistics.module.css"
+import PropTypes from 'prop-types';
+
+export const Statistics = ({stats,title})=>{
+
+  return <section className={css.statistics}>
+     <h2 className={css.title}>{title}</h2>
+     <ul className={css.stat_list}>
+     {stats.map(stat => <li key={stat.id} className={css.item}>
+       <span className={css.label}>{stat.label}</span>
+       <span className={css.percentage}>{stat.percentage}</span>
+     </li>)}
+     </ul>
+     </section>
+     }
+
+     Statistics.propTypes = {
+      title: PropTypes.string.isRequired,
+      stats: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          label: PropTypes.string.isRequired,
+          percentage: PropTypes.number.isRequired,
+        })
+      ).isRequired,
+    };
